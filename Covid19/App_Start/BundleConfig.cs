6e26064205f88dc2c21a901c89d84e3718a57016
Gabriel Alcara <gabriel.alcara@telefonica.com>
2020-09-18 13:50:53 -0300

    Add project files.

---
+++ Covid19/App_Start/BundleConfig.cs
@@ -0,0 +1,41 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Covid19
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+               "~/Scripts/DataTables/jquery.dataTables.min.js",
+               "~/Scripts/DataTables/dataTables.bootstrap.min.js",
+               "~/Scripts/DataTables/dataTables.buttons.min.js",
+               "~/Scripts/DataTables/dataTables.fixedColumns.min.js",
+               "~/Scripts/DataTables/buttons.html5.min.js",
+               "~/Scripts/DataTables/buttons.bootstrap.min.js"));
+
+            //CSS
+            bundles.Add(new StyleBundle("~/Content/datatables/css/datatables").Include("~/Content/DataTables/css/dataTables.bootstrap.min.css", "~/Content/DataTables/css/buttons.dataTables.css"));
+        }
+    }
+}
---
+++ Covid19/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.UI.WebControls.Expressions;
+
+namespace Covid19.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public JsonResult SearchAPI()
+        {
+            var requisicaoWeb = WebRequest.CreateHttp("https://elastic-leitos.saude.gov.br/leito_ocupacao/_search");
+            requisicaoWeb.Method = "GET";
+            requisicaoWeb.UserAgent = "Covid";
+            requisicaoWeb.Credentials = new NetworkCredential("user-api-leitos", "aQbLL3ZStaTr38tj");
+            using (var resposta = requisicaoWeb.GetResponse())
+            {
+                var streamDados = resposta.GetResponseStream();
+                StreamReader reader = new StreamReader(streamDados);
+                object objResponse = reader.ReadToEnd();
+                var search = JsonConvert.DeserializeObject<Search>(objResponse.ToString());
+                var hits = JsonConvert.DeserializeObject<Hits>(objResponse.ToString());
+                streamDados.Close();
+                resposta.Close();
+
+                return Json(new { Search = search, Hits = hits }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public class Search
+        {
+            public int Took { get; set; }
+            public string TimeOut { get; set; }
+        }
+        public class Hits
+        {
+            public int Total { get; set; }
+            public string Relation { get; set; }
+            public int MaxScore { get; set; }
+        }
+    }
+}
